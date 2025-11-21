3b609c34ca7d918910d1bd10938bc59f0c0eaf8d
“Rhizobium-gitsgit config --global user.email “okay.bio.sato@gmail.com <okay.bio.sato@gmail.com>
2025-11-22 03:26:42 +0900

    コード修正

--- src/pages/index-en.js
+++ src/pages/index-en.js
@@ -134,10 +134,17 @@ export default function Home() {
             style={{ width: '32px', height: '32px', objectFit: 'contain' }}
           />
         </a>
+        <a href="https://scholar.google.com/citations?user=13x6x4sAAAAJ&hl=ja&authuser=2" target="_blank" rel="noopener noreferrer">
+          <img 
+            src="/images/scholar-icon.png" 
+            alt="Google Scholar" 
+            style={{ width: '32px', height: '32px', objectFit: 'contain' }}
+          />
+        </a>
       </div>
 
       <p style={{ fontSize: '12px', textAlign: 'right' }}>
-        Last updated: {new Date().toLocaleDateString()}<br />
+        Last updated: {new Date().toLocaleDateString('ja-JP')}<br />
         E-mail: sato.mb@keio.jp (Keio University) / t-sato.csb@tmd.ac.jp (Institute of Science Tokyo)
       </p>
     </div>
