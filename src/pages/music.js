f49c36a1d9e848e415854f3baef18e3a9dcc84b4
“Rhizobium-gitsgit config --global user.email “okay.bio.sato@gmail.com <okay.bio.sato@gmail.com>
2025-11-22 16:21:13 +0900

    I Love Unchi動画追加

---
+++ src/pages/music.js
@@ -0,0 +1,14 @@
+<div style={{ marginBottom: '40px' }}>
+          <h3 style={{ fontSize: '18px', marginBottom: '15px' }}>I Love Unchi</h3>
+          <div style={{ 
+            border: '1px solid #ccc', 
+            padding: '20px',
+            borderRadius: '8px'
+          }}>
+            <video controls style={{ width: '100%', marginBottom: '10px' }}>
+              <source src="/music/I Love Unchi.mp4" type="video/mp4" />
+              お使いのブラウザは動画再生に対応していません。
+            </video>
+          </div>
+        </div>
+
